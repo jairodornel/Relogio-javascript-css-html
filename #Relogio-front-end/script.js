3a95b0ed184a8cced22080c00783d1f35c64c5e5
jairodornel <89214328+jairodornel@users.noreply.github.com>
2022-11-02 22:40:24 -0300

    pacote

---
+++ #Relogio-front-end/script.js
@@ -0,0 +1,33 @@
+const hourHand = document.querySelector(".hand.hours");
+const minutesHand = document.querySelector(".hand.minutes");
+const secondsHand = document.querySelector(".hand.seconds");
+
+const setRotation = (element,rotationPercentage) => {
+    element.style.setProperty("--rotation",rotationPercentage *360);
+
+
+};
+
+const setClock = ()=> {
+
+    const currentDate = new Date();
+
+    const secondsPercentage = currentDate.getSeconds() /60;
+
+    const minutesPercentage = currentDate.getMinutes() /60;
+
+    const hoursPercentage = currentDate.getHours() /12 ;
+
+    setRotation(secondsHand,secondsPercentage);
+    setRotation(minutesHand,minutesPercentage);
+    setRotation(hourHand,hoursPercentage);
+
+
+
+
+};
+
+setClock();
+
+setInterval( setClock, 1000);
+
